364213ef27d5aabba9f5247629a9208a4194d17b
Nick <b10303008@gmail.com>
2020-08-22 14:35:32 +0800

    [feature] settle viewport addon in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,35 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  Laptop: {
+    name: 'Laptop',
+    styles: {
+      width: '1440px',
+      height: '700px',
+    },
+  },
+  LaptopNoFullScreen: {
+    name: 'Laptop (when no full screen)',
+    styles: {
+      width: '1440px',
+      height: '600px',
+    },
+  },
+  Desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1960px',
+      height: '800px',
+    },
+  }
+};
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+     ...customViewports,
+   },
+  }
 }
