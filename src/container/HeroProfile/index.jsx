dca60e23410f3a096f5b0c02893170b401607feb
Nick <b10303008@gmail.com>
2020-08-23 01:49:11 +0800

    [feature] create HeroProfile container

---
+++ src/container/HeroProfile/index.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import HeroProfile from "../../components/HeroProfile/HeroProfile";
+import { REQUEST_STATE } from "../../common/constants";
+
+const fetchHeroProfile = (id) => {
+  return new Promise((resolve) => {
+    fetch(`https://hahow-recruit.herokuapp.com/heroes/${id}/profile`)
+      .then((response) => response.json())
+      .then(resolve);
+  });
+};
+
+const updateHeroProfile = (id, data) => {
+  return new Promise((resolve) => {
+    fetch(`https://hahow-recruit.herokuapp.com/heroes/${id}/profile`, {
+      body: JSON.stringify(data),
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then(resolve);
+  });
+};
+
+const useHeroProfile = (id) => {
+  const [state, setState] = useState(REQUEST_STATE.LOADING);
+  const [profile, setProfile] = useState({});
+  const [remainPoints, setRemainPoints] = useState(0);
+
+  const handleIncrease = (key) => {
+    setRemainPoints(remainPoints - 1);
+    setProfile({
+      ...profile,
+      [key]: profile[key] + 1,
+    });
+  };
+
+  const handleDecrease = (key) => {
+    const value = profile[key];
+    if (value < 1) {
+      throw Error("handleDecrease: Value can't be negative");
+    }
+    setRemainPoints(remainPoints + 1);
+    setProfile({
+      ...profile,
+      [key]: value - 1,
+    });
+  };
+
+  useEffect(() => {
+    const handleFetchSuccess = (data) => {
+      setProfile(data);
+      setState(REQUEST_STATE.SUCCESS);
+    };
+
+    const handleFetchFailure = (error) => {
+      console.error(error);
+      setState(REQUEST_STATE.FAILURE);
+    };
+
+    fetchHeroProfile(id).then(handleFetchSuccess).catch(handleFetchFailure);
+  }, [id]);
+
+  return {
+    state,
+    profile,
+    handleIncrease,
+    handleDecrease,
+    remainPoints,
+  };
+};
+
+const Container = (props) => {
+  const { id } = props;
+  const [isSubmiting, setIsSubmiting] = useState(false);
+
+  const {
+    state,
+    profile,
+    handleIncrease,
+    handleDecrease,
+    remainPoints,
+  } = useHeroProfile(id);
+
+  const spinnerDatas = Object.keys(profile).map((key) => {
+    const value = profile[key];
+    return {
+      name: key,
+      text: key.toUpperCase(),
+      value: value,
+      disabledDecrease: value === 0,
+      disabledIncrease: remainPoints === 0,
+      onIncrease: handleIncrease,
+      onDecrease: handleDecrease,
+    };
+  });
+
+  const handleSubmit = () => {
+    setIsSubmiting(true);
+    updateHeroProfile(id, profile)
+      .catch(console.error)
+      .finally(() => {
+        setIsSubmiting(false);
+      });
+  };
+
+  return (
+    <HeroProfile
+      state={state}
+      spinnerDatas={spinnerDatas}
+      remainPoints={remainPoints}
+      onSubmit={handleSubmit}
+      isDisabledSubmit={isSubmiting || remainPoints !== 0}
+    />
+  );
+};
+
+Container.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+
+export default Container;
---
+++ src/container/HeroProfile/index.stories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import HeroProfileContainer from "./index";
+
+export default {
+  title: "Container/HeroProfile",
+  component: HeroProfileContainer,
+};
+
+const Template = (args) => <HeroProfileContainer {...args} />;
+export const RealAPI = Template.bind({});
+RealAPI.args = {
+    id: 1
+}
